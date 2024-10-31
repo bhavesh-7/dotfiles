eba5d1c1aa789626c13a1ebea72ba84b96692e7f
bhavesh-7 <rohitommishra43@gmail.com>
2024-10-31 18:04:19 +0530

    idk why!

--- nvim/lua/vim-options.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.g.mapleader = " "
-vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
-
